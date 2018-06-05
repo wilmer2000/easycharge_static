624c3228038e77dfecb6938c712c2b2c1bd29cb4
Wilmer Blanco <wilmervzla2000@gmail.com>
2018-06-05 00:55:34 -0300

    new cambios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	var urlMapAnunciate = 'https://www.google.com/maps/d/embed?mid=1Xme3wdFg6Rzj1I4dJpd8YppjYdFNy8Qp';
+	var urlMapAlquileres = 'https://www.google.com/maps/d/embed?mid=13XlGv_SixkLzIBchLpTy8BYn4Do_7NPV';
 	var shrinkHeader = 100; 
 	$(window).scroll(function () {
 	    var scroll = getCurrentScroll();
@@ -45,9 +47,16 @@ $(document).ready(function(){
     });
     $('.tabs-list a').on('click touchstart', function(event){
         event.preventDefault();
-        var id = $(this).attr('id');
-        $('.tabs-list a.active, .tabs-section .tab.active').removeClass('active');
+		var id = $(this).attr('id');
+		$('.mapa_tab #map').attr('src', '');
+		$('.tabs-list a.active, .tabs-section .tab.active').removeClass('active');
         $(this).addClass('active');
-        $('.tabs-section .tab' + id).addClass('active');
+		$('.tabs-section .tab' + id).addClass('active');
+		if (id === '#one') {
+			$('.mapa_tab #map').attr('src', urlMapAlquileres);
+		}
+		if (id === '#three') {
+			$('.mapa_tab #map').attr('src', urlMapAnunciate);
+		}
     });
 });
